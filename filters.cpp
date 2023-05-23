8bcfc43eb965fa47c3b8d172df19501f71691c80
Nicolas <nicolas.maruyama@gmail.com>
2023-05-22 22:19:24 -0300

    Agrego blackWhite 22/5 - Beta

---
+++ filters.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <math.h>
+#include <vector>
+#include "filters.h"
+#include <thread>  
+#include <atomic>  
+
+#define BLACK 0
+
+using namespace std;
+
+// Filtro plano como ejemplo
+void plain(ppm& img, unsigned char c){
+	for(int i = 0; i < img.height; i++)
+		for(int j = 0; j < img.width; j++)			
+			img.setPixel(i, j, pixel(c,c,c));
+
+}
+
+void blackWhite(ppm& img) {
+    for(int i = 0; i < img.height; i++) {
+        for(int j = 0; j < img.width; j++) {
+            pixel p;
+            p = img.getPixel(i, j);
+            int g = (p.r + p.g + p.b)/3;		
+            img.setPixel(i, j, pixel(g, g, g));
+        }
+    }
+}
+
+
+
+// COMPLETAR :)
+
+
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include "filters.h"  // Incluye el archivo de encabezado "filters.h"
+#include <thread>
+#include <unistd.h>
+#include <fstream>
+
+#define ONE_OVER_BILLION 1E-9  // Define una constante con el valor 1E-9 (1 dividido por mil millones)
+
+using namespace std;
+
+// El siguiente es un template básico que pueden usar como base
+
+int main(int argc, char* argv[]) {
+	
+	// Asumimos que los filtros sin p1 se escriben primero (por lo tanto, el primer p1 no es nulo)
+	// Asumimos que Zoom no se puede encadenar
+
+	if (string(argv[1]) == "-help") {
+		cout << "Uso: ./main <filtro> <nthreads> <[p1]> <img1> <custom_output> <[p2]> <img2>" << endl;
+		return 0; 
+	}
+	
+	string filter = string(argv[1]);    // Almacena el primer argumento en una variable de tipo string llamada "filter"
+	unsigned int n = atoi(argv[2]);     // Convierte el segundo argumento a un entero sin signo y lo almacena en "n"
+	float p1 = atof(argv[3]);           // Convierte el tercer argumento a un número de punto flotante y lo almacena en "p1"
+	string img1(argv[4]);                // Crea un objeto de tipo string llamado "img1" inicializado con el cuarto argumento
+	string out = string(argv[5]);        // Crea un objeto de tipo string llamado "out" inicializado con el quinto argumento
+	
+	ppm img(img1);                       // Crea un objeto de tipo "ppm" llamado "img" y lo inicializa con el valor de "img1"
+	
+	cout << "Aplicando filtros" << endl;
+
+	if (filter == "plain")
+		plain(img, (unsigned char)p1);   // Aplica el filtro "plain" a la imagen utilizando el valor de "p1" como parámetro
+
+	cout << "Escribiendo imagen" << endl;
+	img.write(out);                      // Escribe la imagen resultante en el archivo especificado por "out"	
+	
+	cout << "Listo" << endl;
+	return 0;
+}
