56e5f0df94a4a28edd8411fcd67b36cd1a5311a6
Nicolas <nicolas.maruyama@gmail.com>
2023-06-05 22:21:20 -0300

    05/06 - 22:21
    Co-authored-by: Fernando Reynoso Purizaca <Purinoso@users.noreply.github.com>
    Co-authored-by: AgusIV007 <AgusIV007@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <fstream>
 #include <any>
+#include <map>
+#include <filters.cpp>
 
 #define ONE_OVER_BILLION 1E-9 // Define una constante con el valor 1E-9 (1 dividido por mil millones)
 
@@ -16,7 +18,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-
 	// Asumimos que los filtros sin p1 se escriben primero (por lo tanto, el primer p1 no es nulo)
 	// Asumimos que Zoom no se puede encadenar
 
@@ -25,7 +26,7 @@ int main(int argc, char *argv[])
 		cout << "Uso: ./main <filtro> <nthreads> <[p1]> <img1> <custom_output> <[p2]> <img2>" << endl;
 		return 0;
 	}
-
+	
 	string filter = string(argv[1]); 		// Almacena el primer argumento en una variable de tipo string llamada "filter"
 	unsigned int nthreads = atoi(argv[2]);	// Convierte el segundo argumento a un entero sin signo y lo almacena en "n"
 	float p1 = atof(argv[3]);		 		// Convierte el tercer argumento a un número de punto flotante y lo almacena en "p1"
@@ -34,47 +35,16 @@ int main(int argc, char *argv[])
 	string out = string(argv[5]); // Crea un objeto de tipo string llamado "out" inicializado con el quinto argumento
 
 	ppm img(img1); // Crea un objeto de tipo "ppm" llamado "img" y lo inicializa con el valor de "img1"
-	ppm img_2(img2);
+	ppm img2(img2);
+
+	//https://www.geeksforgeeks.org/passing-a-function-as-a-parameter-in-cpp/
+	
+	map<string, void> map;
+	map["blackWhite"] = blackWhite;
 
 	cout << "Aplicando filtros" << endl;
-
 	if (nthreads = 1) {
-		if (filter == "plain")
-		{
-			plain(img, (unsigned char)p1); // Aplica el filtro "plain" a la imagen utilizando el valor de "p1" como parámetro
-		}
-		else if (filter == "blackWhite")
-		{
-			blackWhite(img);
-		}
-		else if (filter == "shades")
-		{
-			shades(img, p1);
-		}
-		else if (filter == "brightness")
-		{
-			brightness(img, p1, 0, 0);
-		}
-		else if (filter == "merge")
-		{
-			merge(img, img_2, p1);
-		}
-		else if (filter == "boxBlur")
-		{
-			boxBlur(img);
-		}
-		else if (filter == "edgeDetection")
-		{
-		edgeDetection(img);
-		}
-		else if (filter == "sharpen")
-		{
-		sharpen(img);
-		}
-		else if (filter == "contrast")
-		{
-			contrast(img, p1);
-		}
+		applyFilter(filter, img1, img2, p1);
 	} else if (nthreads > 1) {
 		vector<ppm> vecImages = threadsImageDivision(img, nthreads);
 		for (int threads; threads < nthreads; threads++) {
